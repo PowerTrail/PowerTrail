b9a835217674fc02523dced6f77ac420d17deade
RIDDHI SHAH <123880722+Riddhi554@users.noreply.github.com>
2025-03-31 20:52:05 +0530

    for video filesssss (#12)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- types/powerGrid.ts
+++ types/powerGrid.ts
@@ -26,12 +26,15 @@ export interface WindFarm {
   substation: string;
   installed_capacity: number;
   latitude: number;
+  grid_connection?: string;
+  capacity: number;
   longitude: number;
   turbine_count?: number;
   avg_wind_speed?: string;
   annual_generation?: string;
-  commissioned_date?: string;
+  commissioned_date: string;
   maintenance_schedule?: string;
+  turbine_model?: string; 
 }
 
 export interface SolarPark {
@@ -40,7 +43,12 @@ export interface SolarPark {
   total_capacity_dc: number;
   total_capacity_ac: number;
   latitude: number;
+  capacity?: number;
   longitude: number;
+  commissioned_date : string;
+  panel_type?: string;
+  grid_connection?: string;
+  maintenance_schedule?: string;
   connectivity_details?: {
     transformers?: string;
     mainLines?: string[];
